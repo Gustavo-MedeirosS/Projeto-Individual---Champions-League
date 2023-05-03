a3615d8d90d5838b36b611c83efb73e367610795
Gustavo-Medeiros <gustavo.msilva@sptech.school>
2023-05-03 00:00:32 -0300

    Atualização Home

--- js/carrossel.js
+++ js/carrossel.js
@@ -1,17 +1,17 @@
-let count = 1;
-document.getElementById("radio1").checked = true;
+// let count = 1;
+// document.getElementById("radio1").checked = true;
 
-setInterval(function () {
-  nextImage();
-}, 5000);
+// setInterval(function () {
+//   nextImage();
+// }, 5000);
 
-function nextImage() {
-  count++;
-  if (count > 7) {
-    count = 1;
-  }
-  document.getElementById("radio" + count).checked = true;
-}
+// function nextImage() {
+//   count++;
+//   if (count > 7) {
+//     count = 1;
+//   }
+//   document.getElementById("radio" + count).checked = true;
+// }
 
 function addElement(description, imageUrl, isFirst) {
   console.log("Chamou");
@@ -24,10 +24,10 @@ function addElement(description, imageUrl, isFirst) {
   document.getElementById("carrossel").innerHTML += conteudo;
 }
 
-addElement("teste", "../images/home/sorteio-fase-de-grupos.jpeg", true);
-addElement("teste", "../images/home/confronto-equipes.jpeg", false);
-addElement("teste", "../images/home/classificacao-fase-de-grupos.jpg", false);
-addElement("teste", "../images/home/chaveamento-mata-mata-champions.png", false);
-addElement("teste", "../images/home/foto-classificacao-messi.jpg", false);
-addElement("teste", "../images/home/taca-champions-foto.jpg", false);
-addElement("teste", "../images/home/taca-mundial-de-clubes.jpg", false);
+addElement("32 equipes distribuídas em 8 grupos, sendo 4 em cada grupo", "../images/home/sorteio-fase-de-grupos.jpeg", true);
+addElement("As equipes se enfrentam 2 vezes na fase de grupos, totalizando 6 jogos para cada time", "../images/home/confronto-equipes.jpeg", false);
+addElement("As 2 equipes que mais pontuarem em seus grupos se classificam para a fase mata-mata", "../images/home/classificacao-fase-de-grupos.jpg", false);
+addElement("As fases do mata-mata são: oitavas de final, quartas de final, semifinal e final", "../images/home/chaveamento-mata-mata-champions.png", false);
+addElement("No mata-mata, as equipes se enfrentam em jogo de ida e volta, se classificando quem for vncedor no resultado agregado final ou nos pênaltis, caso haja empate", "../images/home/foto-classificacao-messi.jpg", false);
+addElement("A final é decidida em jogo único e em campo neutro. O vencedor leva a orelhuda pra casa!", "../images/home/taca-champions-foto.jpg", false);
+addElement("O campeão garante vaga no Mundial de Clubes, torneio disputado entre todos os campeões cntinentais do ano", "../images/home/taca-mundial-de-clubes.jpg", false);

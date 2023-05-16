674b69490dd61f95f03b22000022800266b7b275
Gustavo-Medeiros <gustavo.msilva@sptech.school>
2023-05-15 22:04:30 -0300

    Atualização tela Times

---
+++ js/times.js
@@ -0,0 +1,307 @@
+var times = [
+  {
+    imagem: "..//images/times/ajax.png",
+    nomeTime: "Ajax FC",
+    bandeiraPais: "../images/paises/holanda.png",
+    nomePais: "Holanda",
+    qtdTitulos: "4",
+    estadio: "Johan Cruijff Arena",
+  },
+  {
+    imagem: "..//images/times/atletico-madrid.png",
+    nomeTime: "Atlético de Madrid",
+    bandeiraPais: "../images/paises/espanha.png",
+    nomePais: "Espanha",
+    qtdTitulos: "0",
+    estadio: "Estádio Metropolitano",
+  },
+  {
+    imagem: "..//images/times/barcelona.png",
+    nomeTime: "FC Barcelona",
+    bandeiraPais: "../images/paises/espanha.png",
+    nomePais: "Espanha",
+    qtdTitulos: "5",
+    estadio: "Camp Nou",
+  },
+  {
+    imagem: "..//images/times/bayer-leverkusen.png",
+    nomeTime: "Bayer Leverkusen",
+    bandeiraPais: "../images/paises/alemanha.png",
+    nomePais: "Alemanha",
+    qtdTitulos: "0",
+    estadio: "BayArena",
+  },
+  {
+    imagem: "..//images/times/bayern-munique.png",
+    nomeTime: "Bayern de Munique",
+    bandeiraPais: "../images/paises/alemanha.png",
+    nomePais: "Alemanha",
+    qtdTitulos: "6",
+    estadio: "Allianz Arena",
+  },
+  {
+    imagem: "..//images/times/benfica.png",
+    nomeTime: "Benfica",
+    bandeiraPais: "../images/paises/portugal.png",
+    nomePais: "Portugal",
+    qtdTitulos: "2",
+    estadio: "Estádio do SL e Benfica",
+  },
+  {
+    imagem: "..//images/times/borussia-dortmund.png",
+    nomeTime: "Borussia Dortmund",
+    bandeiraPais: "../images/paises/alemanha.png",
+    nomePais: "Alemanha",
+    qtdTitulos: "1",
+    estadio: "Signal Iduna Park",
+  },
+  {
+    imagem: "..//images/times/brugge.png",
+    nomeTime: "Club Brugge",
+    bandeiraPais: "../images/paises/belgica.png",
+    nomePais: "Bélgica",
+    qtdTitulos: "0",
+    estadio: "Jan Breydel Stadium",
+  },
+  {
+    imagem: "..//images/times/celtic.png",
+    nomeTime: "Celtic FC",
+    bandeiraPais: "../images/paises/escocia.png",
+    nomePais: "Escócia",
+    qtdTitulos: "1",
+    estadio: "Celtic Park",
+  },
+  {
+    imagem: "..//images/times/chelsea.png",
+    nomeTime: "Chelsea FC",
+    bandeiraPais: "../images/paises/inglaterra.png",
+    nomePais: "Inglaterra",
+    qtdTitulos: "2",
+    estadio: "Stamford Bridge",
+  },
+  {
+    imagem: "..//images/times/copenhagen.png",
+    nomeTime: "FC Copenhaguen",
+    bandeiraPais: "../images/paises/dinamarca.png",
+    nomePais: "Dinamarca",
+    qtdTitulos: "0",
+    estadio: "Estádio Parken",
+  },
+  {
+    imagem: "..//images/times/dinamo-zagreb.png",
+    nomeTime: "GNK Dinamo Zagreb",
+    bandeiraPais: "../images/paises/croacia.png",
+    nomePais: "Croácia",
+    qtdTitulos: "0",
+    estadio: "Estádio Maksimir",
+  },
+  {
+    imagem: "..//images/times/frankfurt.png",
+    nomeTime: "Eintracht Frankfurt",
+    bandeiraPais: "../images/paises/alemanha.png",
+    nomePais: "Alemanha",
+    qtdTitulos: "0",
+    estadio: "Deutsche Bank Park",
+  },
+  {
+    imagem: "..//images/times/internazionale.png",
+    nomeTime: "Inter de Milão",
+    bandeiraPais: "../images/paises/italia.png",
+    nomePais: "Itália",
+    qtdTitulos: "3",
+    estadio: "Giuseppe Meazza",
+  },
+  {
+    imagem: "..//images/times/juventus.png",
+    nomeTime: "Juventus FC",
+    bandeiraPais: "../images/paises/italia.png",
+    nomePais: "Itália",
+    qtdTitulos: "2",
+    estadio: "Juventus Stadium",
+  },
+  {
+    imagem: "..//images/times/liverpool.png",
+    nomeTime: "Liverpool FC",
+    bandeiraPais: "../images/paises/inglaterra.png",
+    nomePais: "Inglaterra",
+    qtdTitulos: "6",
+    estadio: "Anfield",
+  },
+  {
+    imagem: "..//images/times/maccabi-haifa.png",
+    nomeTime: "Maccabi Haifa FC",
+    bandeiraPais: "../images/paises/israel.png",
+    nomePais: "Israel",
+    qtdTitulos: "0",
+    estadio: "Estádio Sammy Ofer",
+  },
+  {
+    imagem: "..//images/times/manchester-city.png",
+    nomeTime: "Manchester City FC",
+    bandeiraPais: "../images/paises/inglaterra.png",
+    nomePais: "Inglaterra",
+    qtdTitulos: "0",
+    estadio: "Etihad Stadium",
+  },
+  {
+    imagem: "..//images/times/milan.png",
+    nomeTime: "AC Milan",
+    bandeiraPais: "../images/paises/italia.png",
+    nomePais: "Itália",
+    qtdTitulos: "7",
+    estadio: "San Siro",
+  },
+  {
+    imagem: "..//images/times/napoli.png",
+    nomeTime: "SSC Napoli",
+    bandeiraPais: "../images/paises/italia.png",
+    nomePais: "Itália",
+    qtdTitulos: "0",
+    estadio: "Diego Armando Maradona",
+  },
+  {
+    imagem: "..//images/times/olympique-marseille.png",
+    nomeTime: "Olympique de Marseille",
+    bandeiraPais: "../images/paises/franca.png",
+    nomePais: "França",
+    qtdTitulos: "0",
+    estadio: "Stade Vélodrome",
+  },
+  {
+    imagem: "..//images/times/paris-saint-germain.png",
+    nomeTime: "Paris Saint-Germain",
+    bandeiraPais: "../images/paises/franca.png",
+    nomePais: "França",
+    qtdTitulos: "0",
+    estadio: "Parc des Princes",
+  },
+  {
+    imagem: "..//images/times/porto.png",
+    nomeTime: "FC Porto",
+    bandeiraPais: "../images/paises/portugal.png",
+    nomePais: "Portugal",
+    qtdTitulos: "2",
+    estadio: "Estádio do Dragão",
+  },
+  {
+    imagem: "..//images/times/rangers.png",
+    nomeTime: "Rangers FC",
+    bandeiraPais: "../images/paises/escocia.png",
+    nomePais: "Escócia",
+    qtdTitulos: "0",
+    estadio: "Ibrox Stadium",
+  },
+  {
+    imagem: "..//images/times/rb-leipzig.png",
+    nomeTime: "RB Leipzig",
+    bandeiraPais: "../images/paises/alemanha.png",
+    nomePais: "Alemanha",
+    qtdTitulos: "0",
+    estadio: " Red Bull Arena Leipzig",
+  },
+  {
+    imagem: "..//images/times/rb-salzburg.png",
+    nomeTime: "RB Salzburg",
+    bandeiraPais: "../images/paises/alemanha.png",
+    nomePais: "Alemanha",
+    qtdTitulos: "0",
+    estadio: "Red Bull Arena",
+  },
+  {
+    imagem: "..//images/times/real-madrid.png",
+    nomeTime: "Real Madrid CF",
+    bandeiraPais: "../images/paises/espanha.png",
+    nomePais: "Espanha",
+    qtdTitulos: "14",
+    estadio: "Santiago Bernabeu",
+  },
+  {
+    imagem: "..//images/times/sevilla.png",
+    nomeTime: "Sevilla FC",
+    bandeiraPais: "../images/paises/espanha.png",
+    nomePais: "Espanha",
+    qtdTitulos: "0",
+    estadio: "Ramón Sánchez Pizjuán",
+  },
+  {
+    imagem: "..//images/times/shakhtar-donetsk.png",
+    nomeTime: "Shakhtar Donetsk",
+    bandeiraPais: "../images/paises/ucrania.png",
+    nomePais: "Ucrânia",
+    qtdTitulos: "0",
+    estadio: "Estádio Olímpico de Kiev",
+  },
+  {
+    imagem: "..//images/times/sporting.png",
+    nomeTime: "Sporting CP",
+    bandeiraPais: "../images/paises/portugal.png",
+    nomePais: "Portugal",
+    qtdTitulos: "0",
+    estadio: "Estádio José Alvalade",
+  },
+  {
+    imagem: "..//images/times/tottenham.png",
+    nomeTime: "Tottenham Hotspur",
+    bandeiraPais: "../images/paises/inglaterra.png",
+    nomePais: "Inglaterra",
+    qtdTitulos: "0",
+    estadio: "New Tottenham HF Stadium",
+  },
+  {
+    imagem: "..//images/times/viktoria-plzen.png",
+    nomeTime: "FC Viktoria Plzeň",
+    bandeiraPais: "../images/paises/republica-tcheca.png",
+    nomePais: "Rep. Tcheca",
+    qtdTitulos: "0",
+    estadio: "Doosan Arena",
+  },
+];
+
+function addTimes(
+  imagem,
+  nomeTime,
+  bandeiraPais,
+  nomePais,
+  qtdTitulos,
+  estadio
+) {
+  var time = `<div class="card-time">`;
+  time += `
+    <div class="escudo">
+        <img src="${imagem}" alt="">
+        <span>${nomeTime}</span>
+    </div>
+    <div class="info">
+        <div class="pais">
+            <img src="${bandeiraPais}" alt="">
+            <span>${nomePais}</span>
+        </div>
+        <div class="titulos">
+            <i class="fa-solid fa-trophy fa-lg" style="color: #ffffff;"></i>
+            <span>${qtdTitulos}</span>
+        </div>
+        <div class="estadio">
+            <img src="../images/icon-estadio.png" alt="">
+            <span>${estadio}</span>
+        </div>
+    </div>
+  </div>`;
+
+  document.getElementById("times").innerHTML += time;
+};
+
+for (var i = 0; i < times.length; i++) {
+    addTimes(
+        times[i].imagem,
+        times[i].nomeTime,
+        times[i].bandeiraPais,
+        times[i].nomePais,
+        times[i].qtdTitulos,
+        times[i].estadio
+    );
+
+    // if (i % 4 == 3) {
+    //     document.getElementById("times").innerHTML += `</br>`;
+    // }
+};
+

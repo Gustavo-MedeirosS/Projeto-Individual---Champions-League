06debb1d18125dcd77c167641ad2a47c5274b3c7
Gustavo-Medeiros <gustavo.msilva@sptech.school>
2023-05-18 16:00:09 -0300

    Estilizando login/cadastro

--- web-data-viz/site/src/routes/index.js
+++ web-data-viz/site/src/routes/index.js
@@ -2,31 +2,32 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function (req, res) {
-    // res.render("index", { title: "Express" });
-    res.sendFile('index.html', {
-        root: './public/html'
-      });
+    res.render("index", { title: "Express" });
+    // res.sendFile('index.html', {
+    //     root: './public/html'
+    //   });
 });
 
-router.get("/login", function (req, res) {
-    // res.render("index", { title: "Express" });
-    res.sendFile('login.html', {
-        root: './public/html'
-      });
-});
 
-router.get("/cadastro", function (req, res) {
-    // res.render("index", { title: "Express" });
-    res.sendFile('cadastro.html', {
-        root: './public/html'
-      });
-});
+// router.get("/login", function (req, res) {
+//     res.render("index", { title: "Express" });
+//     // res.sendFile('login.html', {
+//     //     root: './public/html'
+//     //   });
+// });
 
-router.get("/noticias", function (req, res) {
-    // res.render("index", { title: "Express" });
-    res.sendFile('noticias.html', {
-        root: './public/html'
-      });
-});
+// router.get("/cadastro", function (req, res) {
+//     res.render("index", { title: "Express" });
+//     res.sendFile('cadastro.html', {
+//         root: './public/html'
+//       });
+// });
+
+// router.get("/noticias", function (req, res) {
+//     res.render("index", { title: "Express" });
+//     res.sendFile('noticias.html', {
+//         root: './public/html'
+//       });
+// });
 
 module.exports = router;
